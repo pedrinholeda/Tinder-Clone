3546066663381f9437cb87a0ce4426bc2bf2ced8
Pedro Leda <pedro.henrique.leda@gmail.com>
2020-03-17 13:32:47 -0300

    adicionando nome, idade e bio

--- Tinder/View/CombineCardView.swift
+++ Tinder/View/CombineCardView.swift
@@ -21,12 +21,48 @@ class CombineCardView: UIView{
         return imageView
     }()
     
+    let nomeLabel: UILabel = .textBoldLabel(32, textColor: .white)
+    let idadeLabel: UILabel = .textLabel(28, textColor: .white)
+    let fraseLabel : UILabel = .textLabel(18, textColor: .white, numberOfLines: 2)
+    
+    
     override init (frame: CGRect){
         super.init(frame:frame)
         
-       addSubview(fotoImagemView)
+        //ajustando bordas do card
+        layer.borderWidth = 0.3
+        layer.borderColor = UIColor.lightGray.cgColor
+        layer.cornerRadius = 8
+        //fazendo o recorte
+        clipsToBounds = true
+        
+        nomeLabel.text = "Ana Laura"
+        idadeLabel.text = "20"
+        fraseLabel.text = "chama a novinha no zap"
+        
+        nomeLabel.adicionaShadow()
+        idadeLabel.adicionaShadow()
+        fraseLabel.adicionaShadow()
+        
+        addSubview(fotoImagemView)
         
         fotoImagemView.preencherSuperview()
+        
+        let nomeIdadeStackView = UIStackView(arrangedSubviews: [nomeLabel, idadeLabel, UIView()])
+        nomeIdadeStackView.spacing = 12
+        
+        let stackView = UIStackView(arrangedSubviews: [nomeIdadeStackView, fraseLabel])
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        
+        addSubview(stackView)
+        stackView.preencher(
+            top: nil,
+            leading: leadingAnchor,
+            trailing: trailingAnchor,
+            bottom: bottomAnchor,
+            padding: .init(top: 0, left: 16, bottom: 16, right: 16)
+        )
     }
     
     required init?(coder: NSCoder) {
